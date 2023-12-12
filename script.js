667abd97a3b9768d80505bda86652f5463753317
HichamBENKADA <hicham20014@gmail.com>
2023-12-12 12:14:28 -0500

    Project files are linked ready for web development

---
+++ script.js
@@ -0,0 +1,5 @@
+// welcome to DOM manipulation Skill base assissment project.
+
+console.log("Welcome to DOM Skill-Base Assissment")
+
+document.body.appendChild(document.createElement("h1")).textContent ="Welcome to DOM Module Project";
